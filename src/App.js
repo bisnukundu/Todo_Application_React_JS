c079cfe450201cf7d293379877e92f55af86c035
bisnukundu <bisnukundu@gmail.com>
2021-07-07 21:49:56 +0600

    now add button is working

--- src/App.js
+++ src/App.js
@@ -4,24 +4,46 @@ import Form from './components/form';
 import Table from './components/table';
 
 class App extends React.Component {
+
   state = {
-    nam:"",
-    address:"Dhaka",
-    contacts:[
+    nam: "",
+    address: "",
+    contacts: [
       { name: "bisnu", address: "Dinajpur" },
       { name: "Sopon", address: "Dhaka" },
       { name: "Manum", address: "Rangpur" }
     ]
   }
-  nameC(e){
- 
+
+
+  nameC = e => {
+    let na = e.target.value;
+    this.setState({ nam: na });
   }
+
+  addC = e => {
+    let add = e.target.value;
+    this.setState({ address: add });
+  }
+
+  save = e => {
+    // e.preventdefault()
+    e.preventDefault();
+    let obj = { name: this.state.nam, address: this.state.address };
+    let newData = [...this.state.contacts, obj];
+    this.setState({
+      contacts: newData,
+      nam: '',
+      address: ''
+    })
+  }
+
   render() {
     return (
       <div className="container border mx-auto shadow-lg p-5 rounded">
         <div className="row">
           <div className="col-6">
-            <Form nam={this.state.nam} address={this.state.address} change={this.nameC} />
+            <Form sv={this.save} addC={this.addC} nam={this.state.nam} address={this.state.address} change={this.nameC} />
           </div>
           <div className="col-6">
             <Table data={this.state.contacts} />
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,19 +3,19 @@ class Form extends React.Component {
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.props.sv}>
                     <div className="form-group mb-2">
                         
                         <label htmlFor="name">Name</label>
-                        <input onChange={this.props.change} type="email" className="form-control" id="name"  placeholder="Enter Name" />
+                        <input value={this.props.nam} onChange={this.props.change} type="text" className="form-control" id="name"  placeholder="Enter Name" />
                     </div>
 
                     <div className="form-group mb-2">
                         <label htmlFor="phone">Phone</label>
-                        <input type="password" className="form-control" id="phone" placeholder="Enter Phone" />
+                        <input value={this.props.address} onChange={this.props.addC} type="text" className="form-control" id="phone" placeholder="Enter Phone" />
                     </div>
 
-                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <button  type="submit" className="btn btn-primary">Add</button>
                 </form>
             </div>
         )
